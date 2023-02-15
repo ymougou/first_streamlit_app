07972708b055e8623ce742232a6c049576e95f56
ymougou <95179106+ymougou@users.noreply.github.com>
2023-02-15 10:18:23 +0100

    Update streamlit_app.py
    modifs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit
+
+strealit.title("My parents new healthy dinner")
+streamlit.header("Breakfast Menu")
+strealit.text("Omega3 & blueberry Oatmeal")
+strealit.text("Kale, Spinach and Rocket Smoothie")
+strealit.text("Hard-Boiled Free-Range Egg")
 
