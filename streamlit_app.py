70def147867b88024c17e995ad377c09e2dbfd09
ymougou <ymougou>
2024-03-11 15:58:12 +0000

    Comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.title("My parents new healthy dinner")
 streamlit.header("Breakfast Favorites")
 streamlit.text("🥣 Omega3 & blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach and Rocket Smoothie")
-streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+#streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞Avocado toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
